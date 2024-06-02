6236b036f895ef8f906a256b222fe58fa5cb0771
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-06-02 23:11:56 +0530

    fixes some

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
         <p className='text-white text-[20px] mt-[30px]'>NoteMaster - Your Ultimate Note-Taking Companion</p>
 
 
-        <Link to="/signup">
+        <Link to="/login">
           <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >Get Started</button>
         </Link>
